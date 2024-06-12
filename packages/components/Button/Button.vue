0dd41e970554b86e61e0772a461cc7a9f5319356
BRITNEY.姜二丫 <9908104+jy627627@user.noreply.gitee.com>
2024-06-13 03:20:49 +0800

    'test-commit'

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
 // Button.vue, Button.test.tsx(测试用例), type.ts, style.css, constants.ts(常量)
+import type { ButtonProps } from "./type.ts";
+import { ref } from "vue";
 
 defineOptions({
   name: 'ErButton',
 })
 
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button',
+})
+
+const slots = defineSlots()
+
+const _ref = ref<HTMLButtonElement>()
+
 </script>
 
 <template>
-  <button>test</button>
+  <component
+      :is="props.tag"
+      ref="_ref"
+      class="er-button"
+      :type="tag === 'button' ? nativeType : void 0"
+      :disabled="disabled || loading ? true : void 0"
+      :class="{
+      [`er-button--${type}`]: type,
+      [`er-button--${size}`]: size,
+      'is-plain':plain,
+      'is-round':round,
+      'is-circle':circle,
+      'is-disabled':disabled,
+      'is-loading':loading,
+    }"
+  >
+    <slot></slot>
+  </component>
 </template>
 
 <style scoped>
-
+@import "style.css";
 </style>
--- packages/docs/index.md
+++ packages/docs/index.md
@@ -3,7 +3,7 @@
 layout: home
 
 hero:
-  name: "Toy-element"
+  name: "Play-component"
   text: "-"
   tagline: My great project tagline
   actions:
